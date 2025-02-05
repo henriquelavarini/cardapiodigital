ff37edd03ad89291a005734a30af85eb559db29a
henriquelavarini <henrique.lavarini2002@gmail.com>
2025-02-05 19:03:04 -0300

    Backend Cardapio Digital

---
+++ src/main/java/com/cardapio/cardapio/Controller/FoodController.java
@@ -0,0 +1,26 @@
+package com.cardapio.cardapio.Controller;
+
+import com.cardapio.cardapio.food.Food;
+import com.cardapio.cardapio.food.FoodRepository;
+import com.cardapio.cardapio.food.FoodResponseDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+	
+	@Autowired
+	private FoodRepository repository;
+	@GetMapping
+	public List<FoodResponseDTO> getAll() {	
+		
+		List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+		return foodList;
+		
+	}
+}
