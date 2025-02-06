f134bf6f6a08f5af53448caeda6908ddba53f711
henriquelavarini <henrique.lavarini2002@gmail.com>
2025-02-05 21:50:37 -0300

    Adicionado a Origem do Cross

--- src/main/java/com/cardapio/cardapio/Controller/FoodController.java
+++ src/main/java/com/cardapio/cardapio/Controller/FoodController.java
@@ -6,11 +6,7 @@ import com.cardapio.cardapio.food.FoodRequestDTO;
 import com.cardapio.cardapio.food.FoodResponseDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -20,6 +16,7 @@ public class FoodController {
 	@Autowired
 	private FoodRepository repository;
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping
 	public void saveFood(@RequestBody FoodRequestDTO data) {
 		
@@ -28,6 +25,7 @@ public class FoodController {
 		return;
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping
 	public List<FoodResponseDTO> getAll() {	
 		return repository.findAll().stream().map(FoodResponseDTO::new).toList();
